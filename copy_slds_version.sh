a2d39d2a62154a096281489b77b24390bf7ff0f9
Jorg Janke <jorg.janke@accorto.com>
2016-04-21 23:32:47 -0700

    save material

--- copy_slds_version.sh
+++ copy_slds_version.sh
@@ -18,13 +18,28 @@ set -x
 #├── @salesforce-ux/design-tokens@1.3.1
 #├── @salesforce-ux/icons@3.3.0
 
+# save ldart
+#cp -R scss/ldart material/sccs
+#cp scss/lightningdart.scss material/sccs
+
+
 
 SLDS=node_modules/@salesforce-ux/design-system
+TOKENS=node_modules/@salesforce-ux/design-tokens
 
-## Copy to target
 
+## Copy to target - assets
+rm -R www/assets
 cp -R ${SLDS}/assets www
+
+## Copy to target - scss
+rm -R www/scss
 cp -R ${SLDS}/scss www
+
+cp -R ${TOKENS}/dist www/scss
+mv www/scss/dist www/scss/tokens
+sed -i "" "s|@salesforce-ux/design-tokens/dist|tokens|g" www/scss/_design-tokens.scss
+
 
 ## delete not used icon files
 cd www/assets/icons
@@ -43,7 +58,12 @@ rm README
 cd -
 
 
-# copy Logo
+# copy material
+cp -R material/images www/assets
+cp -R material/scripts www/assets
+cp -R material/images www/assets
+cp -R material/styles www/assets
 cp LightningDartLogo.svg www/assets/images
 
-
+cp -R material/scss/ldart www/scss
+cp -R material/scss/lightningdart.scss www/scss
